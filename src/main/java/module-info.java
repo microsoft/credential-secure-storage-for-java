d97b44999e87cd005562c08d677ae42e74706bcf
Simon Abykov <sa12@metaswitch.com>
2022-03-29 11:42:31 +0100

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+module credential.secure.storage {
+    requires java.xml;
+    requires com.sun.jna;
+    requires org.slf4j;
+    requires com.sun.jna.platform;
+
+    exports com.microsoft.a4o.credentialstorage.secret;
+    exports com.microsoft.a4o.credentialstorage.storage;
+}
