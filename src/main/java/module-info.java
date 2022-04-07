5289a81aa666569d181a25c804f1ec3b0629150a
Simon Abykov <sa12@metaswitch.com>
2022-04-07 18:09:53 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root.
 
-module credential.secure.storage {
+module com.microsoft.credentialstorage {
     requires com.sun.jna;
     requires com.sun.jna.platform;
     requires org.slf4j;
