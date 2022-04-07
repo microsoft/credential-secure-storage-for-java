f520e43c9fcf8bb65f0efbfdf06e6b77c98dd059
Simon Abykov <sa12@metaswitch.com>
2022-04-07 17:58:15 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,9 @@
 // Licensed under the MIT license. See License.txt in the project root.
 
 module credential.secure.storage {
-    requires java.xml;
     requires com.sun.jna;
-    requires org.slf4j;
     requires com.sun.jna.platform;
+    requires org.slf4j;
 
     exports com.microsoft.credentialstorage.model;
     exports com.microsoft.credentialstorage;
